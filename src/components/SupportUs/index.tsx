7ea47993f704ac82d540439d1b9264605ef8a5f8
Henri_Mello <henri.franca@hotmail.com>
2023-10-14 15:00:52 -0300

    feat: create banner component

---
+++ src/components/SupportUs/index.tsx
@@ -0,0 +1,17 @@
+const SupportUs = () => {
+  return (
+    <>
+      <div className="collapse bg-base-200">
+        <input type="checkbox" />
+        <div className="collapse-title text-xl font-medium">
+          Assine Nosso Abaixo Assinado Clicando nesse Banner
+        </div>
+        <div className="collapse-content">
+          <p>Link</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default SupportUs
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,10 @@
+import SupportUs from '~/components/SupportUs'
+
 const Home = () => {
-  return <h1>It works and you found me!</h1>
+  ;<>
+    return (<h1>It works and you found me!</h1>
+    <SupportUs />)
+  </>
 }
 
 export default Home
