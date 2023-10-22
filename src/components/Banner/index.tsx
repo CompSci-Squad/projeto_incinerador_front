cca6753b57c41bc76a5ec7a998c38c9aa381e5ff
Henri Mello <125467718+HenriMello@users.noreply.github.com>
2023-10-22 12:09:50 -0300

    feat: create banner component (#5)

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,15 @@
+const Banner = () => {
+  return (
+    <div className="collapse bg-base-200">
+      <input type="checkbox" />
+      <div className="collapse-title text-xl font-medium">
+        Assine Nosso Abaixo Assinado Clicando nesse Banner
+      </div>
+      <div className="collapse-content">
+        <p>Link</p>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,12 @@
+import Banner from '~/components/Banner'
+
 const Home = () => {
-  return <h1>It works and you found me!</h1>
+  return (
+    <>
+      <h1>It works and you found me!</h1>
+      <Banner />
+    </>
+  )
 }
 
 export default Home
