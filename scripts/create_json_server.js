8bc04edf13a162d9536f7a4f16c88d46d2ef5e4d
do-ald533 <doouglas.aldred533@gmail.com>
2023-10-24 11:46:05 -0300

    feat: extending script functionality

--- scripts/create_json_server.js
+++ scripts/create_json_server.js
@@ -1,9 +1,45 @@
 import fs from 'node:fs'
 
+import { faker } from '@faker-js/faker'
+
+function createProduct() {
+  return {
+    id: faker.string.uuid(),
+    title: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    description: faker.commerce.productDescription(),
+    mainImage: faker.image.urlLoremFlickr(),
+    images: [
+      faker.image.urlLoremFlickr(),
+      faker.image.urlLoremFlickr(),
+      faker.image.urlLoremFlickr(),
+    ],
+  }
+}
+
+function createEvent() {
+  return {
+    id: faker.string.uuid(),
+    title: faker.lorem.word(),
+    description: faker.lorem.sentence(),
+    image: faker.image.urlLoremFlickr(),
+    location: {
+      number: faker.location.buildingNumber(),
+      street: faker.location.street(),
+      postalCode: faker.location.zipCode(),
+    },
+    date: faker.date.anytime(),
+  }
+}
+
 const json = {
-  posts: [{ id: 1, title: 'json-server', author: 'typicode' }],
-  comments: [{ id: 1, body: 'some comment', postId: 1 }],
-  profile: { name: 'typicode' },
+  products: [],
+  events: []
+}
+
+for (let i = 0; i < 100; i++) {
+  json.products.push(createProduct())
+  json.events.push(createEvent())
 }
 
 // Write the JSON object to the `db.json` file
