e3f0cb9bef77905e4d662b3b42a9e2dd830f5548
Henri_Mello <henri.franca@hotmail.com>
2023-11-08 09:29:10 -0300

    feat: create collective page

---
+++ src/components/CollectiveSection/index.tsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom'
+
+import { ROUTES } from '~/routes/pages-routes'
+
+type CollectiveSectionProps = {
+  title: string
+  image: string
+  text: string
+  path: ROUTES
+}
+
+const CollectiveSection: React.FC<CollectiveSectionProps> = ({
+  title,
+  image,
+  text,
+  path,
+}) => {
+  return (
+    <Link
+      to={path}
+      className="hover card w-96 shadow-xl transition delay-100 ease-in-out hover:bg-gray-300"
+    >
+      <div className="card-body items-center text-center">
+        <h2 className="card-title leading-3">{title}</h2>
+        <figure className="px-10 pt-4">
+          <img src={image} className="rounded-xl" />
+        </figure>
+        <p>{text}</p>
+      </div>
+    </Link>
+  )
+}
+
+export default CollectiveSection
--- src/pages/collective/index.tsx
+++ src/pages/collective/index.tsx
@@ -1,9 +1,26 @@
+import CollectiveSection from '~/components/CollectiveSection'
+
+import { ROUTES } from '../../routes/pages-routes'
+
 const Collective = () => {
   return (
-    <>
-      <h1>It works and you found me!</h1>
-      <p>pagina do coletivo</p>
-    </>
+    <main>
+      <div className="flex justify-center text-xl font-black">O Coletivo</div>
+      <section className="flex justify-around py-6">
+        <CollectiveSection
+          title="Sessão 1"
+          path={ROUTES.COLLECTIVES1}
+          text="Texto sobre o que terá na sessão 1"
+          image="/images/cachorro_arabe.jpeg"
+        />
+        <CollectiveSection
+          title="Sessão 2"
+          path={ROUTES.COLLECTIVES2}
+          text="Texto sobre o que terá na sessão 2"
+          image="/images/cachorro_arabe.jpeg"
+        />
+      </section>
+    </main>
   )
 }
 
