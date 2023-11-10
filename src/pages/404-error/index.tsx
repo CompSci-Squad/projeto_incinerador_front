78bae4e213a9d2c4c7ac3f912fc79f7e8e973eb5
do-ald533 <douglas.aldred533@gmail.com>
2023-11-10 00:38:35 -0300

    feat: creating 404 page

--- src/pages/404-error/index.tsx
+++ src/pages/404-error/index.tsx
@@ -1,5 +1,24 @@
+import { IoIosArrowBack } from 'react-icons/io'
+import { Link } from 'react-router-dom'
+
+import { ROUTES } from '../../routes/pages-routes'
+
 const Erro404 = () => {
-  return <h1>404</h1>
+  return (
+    <main>
+      <div className="flex p-6">
+        <Link to={ROUTES.HOME} className="rounded-full bg-eco_green p-4">
+          <IoIosArrowBack className=" text-lg text-eco_white" />
+        </Link>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-4">
+        <p className="text-lg">
+          Clique no botão acima para retornar à pagina principal
+        </p>
+        <img src="https://http.cat/images/404.jpg" alt="" />
+      </div>
+    </main>
+  )
 }
 
 export default Erro404
