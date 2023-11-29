c4a988860a94a1893d3a3c5661417df45cb28a22
do-ald533 <douglas.aldred533@gmail.com>
2023-11-28 23:13:50 -0300

    fix: fixing linting errors

--- src/routes/routes/collective.tsx
+++ src/routes/routes/collective.tsx
@@ -1,8 +1,8 @@
 import { RouteObject } from 'react-router-dom'
 
 import Collective from '~/pages/collective'
-import Origin from '~/pages/collective/components/origin'
-import Proposals from '~/pages/collective/components/proposals'
+import Origin from '~/pages/collective/components/Origin'
+import Proposals from '~/pages/collective/components/Proposals'
 
 export const collectiveRoute: RouteObject = {
   path: 'collective',
